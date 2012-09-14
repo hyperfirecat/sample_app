979b0e2f4f792304f8534c3768f25c99aecaa317
hyperfire4590 <hyperfire4590@gmail.com>
2012-09-13 22:22:00 -0400

    Dynamic Title 3 Tests Fail

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,17 +1,28 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
     end
+		it "should have the right title" do
+			visit '/static_pages/home'
+			pages.should have_selector('title',
+					:text => "Ruby on Rails Tutorial Sample App | Home")
+		end
   end
 	
 	describe "Help page" do
 		it "should have the content 'Help'" do
 			visit '/static_pages/help'
 			page.should have_content('Help')
+		end
+		it "should have the right title" do
+			visit '/static_pages/help'
+			pages.should have_selector('title',
+					:text => "Ruby on Rails Tutorial Sample App | Help")
 		end
 	end
 
@@ -20,5 +31,11 @@ describe "Static pages" do
 			visit '/static_pages/about'
 			page.should have_content('About Us')
 		end
+		it "should have the right title" do
+			visit '/static_pages/about'
+			pages.should have_selector('title',
+					:text => "Ruby on Rails Tutorial Sample App | About Us")
+		end
 	end
+
 end
