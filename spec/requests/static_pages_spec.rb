c2f44f9921a28e32d888970b83f3035e9983b03e
hyperfire4590 <hyperfire4590@gmail.com>
2012-09-13 23:26:45 -0400

    Exercise num1 Tests pass

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -38,4 +38,16 @@ describe "Static pages" do
 		end
 	end
 
+	describe "Contact page" do
+		it "should have the content 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_content('Contact')
+		end
+		it "should have the right title" do
+			visit '/static_pages/contact'
+			page.should have_selector('title',
+					:text => "Ruby on Rails Tutorial Sample App | Contact")
+		end
+	end
+
 end
