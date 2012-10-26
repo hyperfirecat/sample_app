35759b8d3bb83fc7c695dfd0cfb21bf163fbc7da
hyperfire4590 <hyperfire4590@gmail.com>
2012-10-25 23:54:23 -0400

    Rest of chapter 8 without part 3 - sign out

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,6 +26,10 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
 
+	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:remember_token) }
+	it { should respond_to(:authenticate) }
+
 	it { should be_valid }
 
 	describe "when name is not present" do
@@ -114,5 +118,10 @@ describe User do
   	before { @user.password = @user.password_confirmation = "a" * 5 }
  		it { should be_invalid }
 	end
+	
+	describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
   
 end
