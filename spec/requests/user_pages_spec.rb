dd63c6f1ffebc3d2c34b93d732a87c835926a8c3
hyperfire4590 <hyperfire4590@gmail.com>
2012-10-31 01:32:18 -0400

    Chapter 9 part 4 is complete, users may be deleted.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
-describe "UserPages" do
+describe "User Pages" do
   subject { page }
   
   describe "index" do
     let(:user) { FactoryGirl.create(:user) }
 
-    before(:each) do
+    before do
       sign_in user
       visit users_path
     end
@@ -27,7 +27,26 @@ describe "UserPages" do
         end
       end
     end
-  end
+	
+		describe "delete links" do
+
+		    it { should_not have_link('delete') }
+
+		    describe "as an admin user" do
+		      let(:admin) { FactoryGirl.create(:admin) }
+		      before do
+		        sign_in admin
+		        visit users_path
+		      end
+
+		      it { should have_link('delete', href: user_path(User.first)) }
+		      it "should be able to delete another user" do
+		        expect { click_link('delete') }.to change(User, :count).by(-1)
+		      end
+		      it { should_not have_link('delete', href: user_path(admin)) }
+		    end
+		  end
+		end
 	
 	describe "profile page" do
 		let(:user) { FactoryGirl.create(:user) }
