5bf8dea065174bd82571e0ccff0117c5f957fcc0
hyperfire4590 <hyperfire4590@gmail.com>
2012-10-30 23:38:01 -0400

    Chapter 9, edit user info, only if logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+	before_filter :signed_in_user, only: [:edit, :update]
 	
 	def show
 		@user = User. find(params[:id])
@@ -18,5 +19,26 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+	
+	def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
   
 end
