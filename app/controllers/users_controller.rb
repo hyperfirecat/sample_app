7939ac1551c808ab022895eb4e591bbd064a5d56
hyperfire4590 <hyperfire4590@gmail.com>
2012-11-07 01:22:03 -0500

    Chapter 10 main done - show microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,13 +61,6 @@ class UsersController < ApplicationController
   end
   
   private
-
-    def signed_in_user
-			unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
     
     def correct_user
       @user = User.find(params[:id])
